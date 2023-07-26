62aff508617b7c82a5690229d70c2fde4784dbce
Luis <navarro.luis@outlook.com>
2023-07-26 12:30:19 -0500

    refactored Venmo class and moved to using only access token for auth, removed email/pass auth

--- venmo-scheduler/venmo-scheduler.py
+++ venmo-scheduler/venmo-scheduler.py
@@ -1,41 +1,32 @@
-from venmo_api import Client
-from request import VRequest
-from helper import init_logger, validate_vars
+from venmo_api import User
+from util import Venmo, init_logger, validate_vars
 import os
+
+def run_lambda(event, context):
+    main()
 
 def main():    
 
     logger = init_logger(__name__)
 
-    login_email = os.environ["LOGIN_EMAIL"]
-    login_password = os.environ["LOGIN_PASSWORD"]
+    access_token = os.environ["ACCESS_TOKEN"]
     request_users = [u.strip() for u in os.environ["REQUEST_USERS"].split(",")]
     request_amount = os.environ.get("REQUEST_AMOUNT", 0)
     request_comment = os.environ["REQUEST_COMMENT"]
     send_request = os.environ.get("SEND_REQUEST", "FALSE").upper() == "TRUE"
 
-    validate_vars(login_email=login_email,
-                 login_password=login_password,
+    validate_vars(access_token=access_token,
                  request_users=request_users,
                  request_amount=request_amount)
 
-    try:
-        access_token = Client.get_access_token(username=login_email,
-                                            password=login_password)
-    except Exception as e:
-        logger.error(str(e))
-        raise
-
-    # Initialize api client
-    client = Client(access_token=access_token)
+    venmo = Venmo(access_token=access_token)
     
     for username in request_users:
-        user = VRequest(username, request_amount, request_comment, client)
-        if user.validate_user():
+        user_found = venmo.validate_user(username)
+        if user_found:
+            user = User.from_json(user_found._json)
             if send_request:
-                user.send_request()
-
-    client.log_out(f"Bearer {access_token}")
+                venmo.send_request(user, request_amount, request_comment)
 
 if __name__ == "__main__":
     main()
